a0cfd4af333bc6867bdfee2fa1fc3f4221d80ea9
irohit427 <irohit427@gmail.com>
2020-10-03 14:12:46 +0530

    Added middleware to createCategory route

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const createCategory = require('../controllers/category/createCategory');
+const isAdmin = require('../middleware/isAdmin');
+const isAuth = require('../middleware/isAuth');
+const { signInRequired } = require('../helpers/signInRequired');
+const userById = require('../controllers/userById');
 
-router.post('/create/category', createCategory);
+router.post('/create/category/:userId', signInRequired, isAuth, isAdmin, createCategory);
+
+router.param('userId', userById);
 
 module.exports = router;
