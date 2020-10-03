d6504e032edca729d5b58ab4334858397d64b88e
irohit427 <irohit427@gmail.com>
2020-10-03 17:06:27 +0530

    Update Product

--- src/routes/product.js
+++ src/routes/product.js
@@ -8,11 +8,14 @@ const productById = require('../controllers/product/productById');
 const createProduct = require('../controllers/product/createProduct');
 const deleteProduct = require('../controllers/product/deleteProduct');
 const readProduct = require('../controllers/product/readProduct');
-
+const updateProduct = require('../controllers/product/updateProduct');
 
 router.post('/product/create/:userId', signInRequired, isAuth, isAdmin, createProduct);
 router.get('/product/:productId', readProduct);
 router.delete('/product/:productId/:userId', signInRequired, isAuth, isAdmin, deleteProduct);
+router.put('/product/:productId/:userId', signInRequired, isAuth, isAdmin, updateProduct);
+
 router.param('userId', userById);
 router.param('productId', productById);
+
 module.exports = router;
