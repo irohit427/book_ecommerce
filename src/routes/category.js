05742969e46ed5e6f71f1c81ed74c46cf4eabd7d
irohit427 <irohit427@gmail.com>
2020-10-03 17:16:55 +0530

    Read Category

--- src/routes/category.js
+++ src/routes/category.js
@@ -5,9 +5,13 @@ const isAdmin = require('../middleware/isAdmin');
 const isAuth = require('../middleware/isAuth');
 const { signInRequired } = require('../helpers/signInRequired');
 const userById = require('../controllers/userById');
+const categoryById = require('../controllers/category/categoryById');
+const readCategory = require('../controllers/category/readCategory');
 
 router.post('/create/category/:userId', signInRequired, isAuth, isAdmin, createCategory);
+router.get('/category/:categoryId', readCategory);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
