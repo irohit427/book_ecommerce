5b21afba4fca539d09823bd1fe663b848362282c
irohit427 <irohit427@gmail.com>
2020-10-04 02:06:17 +0530

    List Products by most popular and newest arrival and List Related Products

--- src/routes/product.js
+++ src/routes/product.js
@@ -9,11 +9,15 @@ const createProduct = require('../controllers/product/createProduct');
 const deleteProduct = require('../controllers/product/deleteProduct');
 const readProduct = require('../controllers/product/readProduct');
 const updateProduct = require('../controllers/product/updateProduct');
+const fetchProducts = require('../controllers/product/fetchProducts');
+const fetchRelatedProducts = require('../controllers/product/fetchRelatedProduct');
 
 router.post('/product/create/:userId', signInRequired, isAuth, isAdmin, createProduct);
 router.get('/product/:productId', readProduct);
 router.delete('/product/:productId/:userId', signInRequired, isAuth, isAdmin, deleteProduct);
 router.put('/product/:productId/:userId', signInRequired, isAuth, isAdmin, updateProduct);
+router.get('/products', fetchProducts);
+router.get('/products/related/:productId', fetchRelatedProducts);
 
 router.param('userId', userById);
 router.param('productId', productById);
